b5d1c026ef5345bb1783a18459f5b350ef011418
Basile Quinchon <basile.quinchon@iadvize.com>
2022-02-28 15:54:15 +0100

    feat: add github action

---
+++ caca.ts
@@ -0,0 +1,14 @@
+class Caca {
+  private type: string;
+  private color: string;
+
+  public constructor(type: string, color: string) {
+    this.type = type;
+    this.color = color;
+    this.init();
+  }
+
+  private init() {
+    console.log(`I pooped a ${this.type} ${this.color}`);
+  }
+}
